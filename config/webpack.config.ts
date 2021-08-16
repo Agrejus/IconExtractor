72f43533f8bfb185c081a289f31648c6b62648c3
James DeMeuse <james.demeuse@gmail.com>
2021-08-16 14:13:39 -0500

    remove console log

---
+++ config/webpack.config.ts
@@ -0,0 +1,70 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+const config: webpack.Configuration = {
+
+    entry: {
+        "server": ["./index.ts"]
+    },
+
+    target: "node",
+    mode: "production",
+
+    node: {
+        __dirname: false
+    },
+
+    // Set the naming convention of our bundles
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../lib')
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".js", ".json"],
+        modules: ['node_modules']
+    },
+
+    // Configure our module loaders
+    module: {
+        rules: [
+            {
+                test: /\.(exe)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.ts?$/,
+                include: path.resolve(__dirname, '../../'),
+                loader: require.resolve("awesome-typescript-loader")
+            },
+
+            {
+                test: /\.(js|jsx|mjs)$/,
+                loader: require.resolve('source-map-loader'),
+                enforce: 'pre',
+                include: path.resolve(__dirname, '../../server'),
+            }
+        ]
+    },
+
+    // Configure any plugins
+    plugins: [
+
+    ],
+    performance: {
+        hints: false,
+    },
+};
+
+export default config;
--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import * as path from 'path';
 import * as os from 'os';
 import { exec } from 'child_process';
+const exeFile = require("./IconExtractor/IconExtractor/bin/Debug/IconExtractor.exe");
 
 const getPlatformIconProcess = () => {
     if (os.type() == 'Windows_NT') {
-        return path.join(__dirname, '../bin/IconExtractor.exe');
+        return path.join(__dirname, `/${exeFile.default}`);
     } 
 
     throw ('This platform (' + os.type() + ') is unsupported =(');
@@ -14,6 +15,8 @@ export const getIcon = (fileNameAndPath: string) => {
     return new Promise((resolve, reject) => {
 
         const exe = getPlatformIconProcess();
+        console.log('PATJ');
+        console.log(exe);
         const command = exec(`${exe} --path=\"${fileNameAndPath}\"`);
 
         command.stdout!.on("data", e => {
